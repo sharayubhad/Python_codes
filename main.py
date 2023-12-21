84aedcc3d4496e68697c1f78df088e2df1f351e3
Sharayu Bhad <140377743+sharayubhad@users.noreply.github.com>
2023-12-21 23:15:51 +0530

    Add files via upload
    login form using python

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello world")
+
+
+
+from tkinter import *       
+def login_form():            
+    global login_window      
+    login_window = Tk()      
+    login_window.title ("Login Form")      
+    login_window.geometry('400x300')       
+    Label(login_window,text='Please Enter username and passowrd below').pack()
+    Label(login_window,text='Enter Username').pack()    
+    Entry(login_window).pack()
+    Label(login_window,text='Enter Password').pack()
+    Entry(login_window,show='*').pack()
+    Button(login_window,text='Login').pack()
+    login_window.mainloop()     
+login_form()    
